7900445cc34c24ad826a3be623a5dca8bdecaa21
root <root@6e422b0d3623>
2020-09-10 01:24:38 +0000

    45

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,37 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot GuT7 = new Robot();
 	public void run() {
-		
+		GuT7.setSpeed(100);
+		for (int i = 0; i < 10; i++) {
+			flatRoof();
+		}
 	}
+public void flatRoof() {
+	
+	GuT7.setX(50);
+	GuT7.penDown();
+	GuT7.move(100);
+	GuT7.turn(90);
+	GuT7.move(30);
+	GuT7.turn(90);
+	GuT7.move(100);
+	GuT7.turn(-90);
+	GuT7.setPenColor(Color.GREEN);
+	GuT7.move(20);
+	GuT7.turn(90);
 }
+
+}
+
+
+
+
+
+
+
