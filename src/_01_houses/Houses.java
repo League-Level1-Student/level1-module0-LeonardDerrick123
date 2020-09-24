2082faf24cd1ae38641e25be4d14772fcd134f15
root <root@6e422b0d3623>
2020-09-24 01:32:21 +0000

    4rghdsaWERTEWQW

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -7,24 +7,37 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	Robot GuT7 = new Robot();
 	public void run() {
+		GuT7.setX(50);
 		GuT7.setSpeed(100);
-		for (int i = 0; i < 10; i++) {
-			flatRoof();
+		for (int i = 0; i < 3; i++) {
+			flatRoof("small",Color.MAGENTA);
+		flatRoof("medium",Color.CYAN);
+		flatRoof("large",Color.YELLOW);
 		}
 	}
-public void flatRoof() {
-	
-	GuT7.setX(50);
+public void flatRoof(String height,Color c ) {
+	int h = 60;
+
+	if (height.equals("small")) {
+		h = 60;
+	}
+	if (height.equals("medium")) {
+		h = 120;
+	}
+	if (height.equals("large")) {
+		h = 250;
+	}
 	GuT7.penDown();
-	GuT7.move(100);
+	GuT7.move(h);
 	GuT7.turn(90);
 	GuT7.move(30);
 	GuT7.turn(90);
-	GuT7.move(100);
+	GuT7.move(h);
 	GuT7.turn(-90);
 	GuT7.setPenColor(Color.GREEN);
 	GuT7.move(20);
-	GuT7.turn(90);
+	GuT7.turn(-90);
+GuT7.setPenColor(Color.BLACK);
 }
 
 }
