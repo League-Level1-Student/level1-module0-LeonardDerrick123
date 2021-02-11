71e16739d2734bd366bc4342bd6c6ec07a9f5232
student <student@ip-172-31-56-199.us-west-2.compute.internal>
2021-02-10 18:32:26 -0800

    23erwqerfrewqedfsre

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(200, 200);
 
 		showNextImage();
 
@@ -51,7 +51,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
@@ -59,10 +59,15 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
-		// 6. Otherwise:
+if (guess.equals("Arnald")) {
+JOptionPane.showMessageDialog(null, "Correct");
+score ++;
+}
+	// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+else {
+	JOptionPane.showMessageDialog(null,"Wrong");
+}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
