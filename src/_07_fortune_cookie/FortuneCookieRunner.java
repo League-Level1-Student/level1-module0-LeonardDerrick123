947adbc02cd180c8ffdb214a80947e7df180802e
student <student@ip-172-31-62-19.us-west-2.compute.internal>
2021-06-09 18:28:38 -0700

    fortune cookie

---
+++ src/_07_fortune_cookie/FortuneCookieRunner.java
@@ -0,0 +1,10 @@
+package _07_fortune_cookie;
+
+import javax.swing.JFrame;
+
+public class FortuneCookieRunner {
+	public static void main(String[] args) {
+        new FrtuneCookie().showButton();
+  }
+
+}
